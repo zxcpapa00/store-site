7df8d405854838a2585a58fda09a556e7b3b1702
Evgeniy K <pgg1337@mail.ru>
2023-08-20 18:07:08 +0300

    add new app users

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+def get_image_path(instance, filename):
+    return 'users/{0}/{1}'.format(instance.user.name, filename)
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+    image = models.ImageField(upload_to=get_image_path, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+
+
+
---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('registration/', register, name='register'),
+]
+
